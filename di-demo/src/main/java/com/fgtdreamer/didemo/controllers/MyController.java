17ba5a8509b0b3f762b67d264679fc411b75a043
nipun_jindal <nipun.jindal@swiggy.in>
2018-08-05 19:04:00 +0530

    Primary Qualifier and safety in defining the qualifier if not using hte primary onegit status

--- di-demo/src/main/java/com/fgtdreamer/didemo/controllers/MyController.java
+++ di-demo/src/main/java/com/fgtdreamer/didemo/controllers/MyController.java
@@ -1,11 +1,19 @@
 package com.fgtdreamer.didemo.controllers;
 
+import com.fgtdreamer.didemo.service.GreetingServiceI;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    //lets use a greetingService
+    GreetingServiceI greetingService;
+
+    public MyController(GreetingServiceI greetingService) {//want to use the Primary/By-Default GreetingService Implementation, no need to specify as on of the GreetingService Class is marked as @Primary
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
         System.out.println("Hello!!");
-        return "foo";
+        return greetingService.sayHello();
     }
 }
--- di-demo/src/main/java/com/fgtdreamer/didemo/controllers/PropertyInjectedController.java
+++ di-demo/src/main/java/com/fgtdreamer/didemo/controllers/PropertyInjectedController.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PropertyInjectedController {
     @Autowired
-    @Qualifier("greetingServiceImpl")// not needed if the identifier name mathces the class reference name you put in the paranthesis!! like here!!
+    @Qualifier("greetingServiceImpl") // not needed if the identifier name mathces the class reference name you put in the paranthesis!! like here!! but definitely required if there is any "@Primary" Qualifier
     public GreetingServiceI greetingServiceImpl; // without using Qualifier, just by making the variable/property name same as the Required Class name!!
 
     public String sayHello(){
